ecc86264a329d3b0e9bd4637c4f8454cb5b4f004
arshad ali <arshad300390@gmail.com>
2023-07-22 18:00:49 +0500

    ok

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,7 +1,8 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<a class="btn btn-success" href="{{ url('/')}}">Back</a>
+<a class="btn btn-success" href="{{ url('/') }}">Back</a>
 <h1>Student Data</h1>
 <table border="1" id="students-table">
     <tr>
@@ -14,26 +15,26 @@
 </table>
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         $.ajax({
-            type:"get",
-            url:"{{ route('getStudents') }}",
-            success:function(data){
-                
-                if( data.students.length >0){
-                    for(let i=0; i<data.students.length; i++){
+            type: "get",
+            url: "{{ route('getStudents') }}",
+            success: function(data) {
+
+                if (data.students.length > 0) {
+                    for (let i = 0; i < data.students.length; i++) {
                         let img = data.students[i]['file'];
-                       $("#students-table").append(`<tr>
-                        <td>`+(i+1)+`</td>
-                        <td>`+(data.students[i]['name'])+`</td>
-                        <td>`+(data.students[i]['email'])+`</td>
-                        <td><img  src ="{{ asset('storage/`+img+`') }}" alt='`+img+`'
-                             width='`+120+`' height='`+80+`'/></td>
-                             <td><a href="{{ route('delStudent') }}?id={{ data.students[i]['id'] }}"/></td>
-                        </tr>`) ;
+                        $("#students-table").append(`<tr>
+                        <td>` + (i + 1) + `</td>
+                        <td>` + (data.students[i]['name']) + `</td>
+                        <td>` + (data.students[i]['email']) + `</td>
+                        <td><img  src ="{{ asset('storage/`+img+`') }}" alt='` + img + `'
+                             width='` + 120 + `' height='` + 80 + `'/></td>
+                            //  <td><a href="{{ route('delStudent') }}"/></td>
+                        </tr>`);
                         console.log(img);
                     }
-                }else{
+                } else {
                     $("#students-table").append("<tr> <td colspan='4'>Data not found</td></tr>");
                 }
             }
